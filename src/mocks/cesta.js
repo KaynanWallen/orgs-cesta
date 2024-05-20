f38f3829eb3eec47d36474c1a69e4a85ff2b3d59
kaynanwallen <wallenkaynan@gmail.com>
2024-05-20 18:37:19 -0300

    v.final

--- src/mocks/cesta.js
+++ src/mocks/cesta.js
@@ -1,5 +1,9 @@
 import logo from '../../assets/logo.png'
-
+import tomate from '../../assets/frutas/Tomate.png';
+import brocolis from '../../assets/frutas/Brócolis.png';
+import batata from '../../assets/frutas/Batata.png';
+import pepino from '../../assets/frutas/Pepino.png';
+import abobora from '../../assets/frutas/Abóbora.png';
 
 const cesta = {
     topo: {
@@ -14,30 +18,30 @@ const cesta = {
         botao: 'Comprar'
     },
     itens: {
-        titulo: "Itens da cesta",
-        lista: [
-          {
-            nome: "Tomate",
-            imagem: tomate,
-          },
-          {
-            nome: "Brócolis",
-            imagem: brocolis,
-          },
-          {
-            nome: "Batata",
-            imagem: batata,
-          },
-          {
-            nome: "Pepino",
-            imagem: pepino,
-          },
-          {
-            nome: "Abóbora",
-            imagem: abobora,
-          }
-        ]
-      }
+      titulo: "Itens da cesta",
+      lista: [
+        {
+          nome: "Tomate",
+          imagem: tomate,
+        },
+        {
+          nome: "Brócolis",
+          imagem: brocolis,
+        },
+        {
+          nome: "Batata",
+          imagem: batata,
+        },
+        {
+          nome: "Pepino",
+          imagem: pepino,
+        },
+        {
+          nome: "Abóbora",
+          imagem: abobora,
+        }
+      ]
+    }
 }
 
 export default cesta
